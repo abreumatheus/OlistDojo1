50955fd0ee384a4d1a182c5e53f8b3b6466eb06b
Juliana Karoline <julianaklulo@gmail.com>
2020-12-09 18:29:44 -0300

    Continuação do CRUD de produtos

--- main.py
+++ main.py
@@ -6,41 +6,81 @@ products = []
 
 def new_product() -> None:
     print('\nCreate new product')
+    
     product = Product()
-    product.set_sku()
-    product.set_name()
-    product.set_price()
-    product.set_description()
+    sku_not_valid = True
+    sku = ""
+    while sku_not_valid:
+        sku = input('Product SKU: ')
+        p = search_product(sku)
+        if not p:
+            sku_not_valid = False
+            break
+        print('SKU invalid, try again')
+    
+    product.set_sku(sku)
+    name = input('Product name: ')
+    product.set_name(name)
+    price = input('Product price: ')
+    product.set_price(price)
+    description = input('Product description: ')
+    product.set_description(description)
+
     products.append(product)
 
 
 def list_products():
-    print('\nList all products')
-    for product in products:
-        print(product)
+    if len(products) > 0:
+        print('\nList all products')
+        for product in products:
+            print(product)
+    else:
+        print('\nNo products available.')
 
 
-def search_product(initial_product: str) -> Product:
+def input_sku():
+    initial_product = input('Enter the product SKU: ')
+    return search_product(initial_product)
+
+
+def search_product(sku: str) -> Product:
     for product in products:
-        if product.get_sku() == initial_product:
+        if product.get_sku() == sku:
             return product
 
 
-def edit_product():
-    initial_product = input('Enter the product SKU: ')
-    p = search_product(initial_product)
-    if p is None:
+def product_detail():
+    p = input_sku()
+    if not p:
         print('Product not found.')
     else:
         print(p)
+
+
+def delete_product():
+    p = input_sku()
+    if not p:
+        print('Product not found.')
+    else:
+        products.remove(p)
+        print('Product deleted successfully')
+
+
+def edit_product():
+    p = input_sku()
+    if not p:
+        print('Product not found.')
+    else:
+        print(p)
+        
         p.set_name()
         p.set_price()
         p.set_description()
 
 
 def menu():
-    options = ['Cadastrar Produto', 'Editar Produto',
-               'Listar Produtos', 'Deletar Produtos', 'Sair']
+    options = ['Cadastrar Produto', 'Editar Produto', 'Listar Produtos', 
+               'Buscar Produto por SKU', 'Deletar Produtos', 'Sair']
 
     print('\nMENU: ')
 
@@ -61,8 +101,10 @@ while True:
         elif op == 3:
             list_products()
         elif op == 4:
-            delete_product()
+            product_detail()
         elif op == 5:
+            delete_product()
+        elif op == 6:
             exit(1)
         else:
             print('Opção indisponível. Tente novamente.')
--- products.py
+++ products.py
@@ -4,29 +4,25 @@ class Product:
     __price: float
     __description: str
 
-    def set_sku(self) -> None:
-        sku = input('Enter the product sku: ')
+    def set_sku(self, sku: str) -> None:
         self.__sku = sku
 
     def get_sku(self) -> str:
         return self.__sku
 
-    def set_name(self) -> None:
-        name = input('Enter the product name: ')
+    def set_name(self, name: str) -> None:
         self.__name = name
 
     def get_name(self) -> str:
         return self.__name
 
-    def set_price(self) -> None:
-        price = float(input('Enter the product price: '))
+    def set_price(self, price: float) -> None:
         self.__price = price
 
     def get_price(self) -> float:
         return self.__price
 
-    def set_description(self) -> None:
-        description = input('Enter the product description: ')
+    def set_description(self, description: str) -> None:
         self.__description = description
 
     def get_description(self) -> str:
