4e39e41704e6c1d465610cb3cfc59a0ae11b5819
Maykon Granemann <maykon.granemann@olist.com>
2020-12-23 11:40:23 -0300

    templates html

--- teoria/p2_funcoes/web.py
+++ teoria/p2_funcoes/web.py
@@ -1,31 +1,45 @@
 # pip3 install flask
 # which pip3 -  caminho da instalacao do pip3
-from flask import Flask
-from calculadora import soma
+from flask import Flask, render_template
+from calculadora import soma, subtracao, multiplicacao, divisao
 
 app = Flask(__name__)
 
+titulo_app = 'Calculadora Olist'
+
 @app.route('/')
 def index():
-    h1 = '<h1> Calculadora Olist </h1>'
-    ol = '''
-            <ol> 
-                <li><a href='/soma'>Somar</a></li>
-                <li><a>Subtrair</a></li> 
-            </ol>
-        '''
-    return f'{h1} {ol}'
+    lista = ['somar', 'subtrair', 'multiplicar', 'dividir']
+    return render_template('index.html', nome=titulo_app, lista=lista )
 
-@app.route('/soma')
+@app.route('/somar')
 def somar():
     n1 = 3.0
     n2 = 5.0
     resultado = soma(n1, n2)
-    h1 = '<h1> Calculadora </h1>'
-    h3 = f'<h3> A soma de 3+5 é : {resultado}</h3>'
-    voltar = "<a href='/'>Voltar</a>"
+    return render_template('somar.html',nome=titulo_app, resultado=resultado)
 
-    return f'{h1} {h3} <br /> {voltar}'
+@app.route('/subtrair')
+def subtrair():
+    n1 = 3.0
+    n2 = 5.0
+    resultado = subtracao(n1, n2)
+    return render_template('subtrair.html',nome=titulo_app, resultado=resultado)
+    
+@app.route('/multiplicar')
+def multiplicar():
+    n1 = 3.0
+    n2 = 5.0
+    resultado = multiplicacao(n1, n2)
+    return render_template('multiplicar.html',nome=titulo_app, resultado=resultado)
 
-app.run()
+@app.route('/dividir')
+def dividir():
+    n1 = 10.0
+    n2 = 3.0
+    resultado = divisao(n1, n2)
+    resultado = f'{resultado:.2f}'
+    return render_template('dividir.html',nome=titulo_app, resultado=resultado)
+
+app.run(debug=True)
 
