945674183d9c5f5673f542469536f062ba01810a
jcosta86 <jeferson.costa@olist.com>
2021-01-29 14:43:00 -0300

    incluindo função para validação de valores positivos

--- aula017/src/utils/validators.py
+++ aula017/src/utils/validators.py
@@ -17,3 +17,9 @@ def validate_len(value: object, max_len: int, key: str):
     if len(value) > max_len:
         raise ValueError(f"{key.capitalize()} can't be greater than {max_len} characters.")
     return value
+
+
+def validate_be_greater_than_zero(value: float, key: str):
+    if value <= 0:
+        raise ValueError(f"Value can't be lower than zero.")
+    return value
