6836b4a458a6d856ffc98e46bbe3af2162031d74
Maykon D Granemann <maykon.granemann@olist.com>
2021-01-29 11:09:02 -0300

    Adicionado pytest.mark.parametrize e pytest.raises

--- aula017/src/tests/models/test_team.py
+++ aula017/src/tests/models/test_team.py
@@ -3,19 +3,57 @@ sys.path.append('.')
 from src.models.team import Team
 import pytest
 
-@pytest.fixture
-def create_object():
-    return Team('as', 'Melhor time do Brasil')
 
-def test_name_not_none(create_object):
+@pytest.mark.parametrize("name, description", [
+                            ('N',''), 
+                            ('N'*100,'D'*255), 
+                            ('N'*50,'D'*120)
+                        ])
+def test_team_instance(name, description):
+    team = Team(name, description)
+    assert isinstance(team, Team)
+
+def test_name_min_len():
+    name = 'C'
+    description = ''
+    team = Team(name, description)
+    assert team.name is name
+
+def test_name_not_none():
     with pytest.raises(TypeError):
-        create_object
-   
+        team = Team(None, 'Melhor time do Brasil')
 
-def test_name_not_str():
-    try:
-        team = Team(10, 'Melhor time do Brasil')
-        raise NotImplementedError('Exception not raised')
-    except Exception as error:
-        assert isinstance(error, TypeError)
+@pytest.mark.parametrize("name, description", [
+                            (10,'Melhor time do Brasil'), 
+                            (10.5,'Melhor time do Brasil'), 
+                            (False,'Melhor time do Brasil')
+                        ])
+def test_name_not_str(name, description):
+    with pytest.raises(TypeError):
+        team = Team(name, description)
 
+def test_name_not_empty():
+    with pytest.raises(ValueError):
+        team = Team('', 'Melhor time do Brasil')
+
+def test_name_max_len():
+    with pytest.raises(ValueError):
+        team = Team('Co'*51, 'Melhor time do Brasil')
+
+def test_description_min_len():
+    name = 'C'
+    description = ''
+    team = Team(name, description)
+    assert team.description is description
+
+def test_description_not_none():
+    with pytest.raises(TypeError):
+        team = Team('Metropolitano', None)
+
+def test_description_not_str():
+    with pytest.raises(TypeError):
+        team = Team('Metropolitano', 10)
+
+def test_description_len():
+    with pytest.raises(ValueError):
+        team = Team('Metropolitano', 'c'*256)
