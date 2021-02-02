a9284d8b27d17e598577a9b6f45f1084658e1668
rafaela.teixeira <rafaellynha.a@hotmail.com>
2021-02-02 15:47:01 -0300

    tests product dao

---
+++ aula017/src/tests/dao/test_product_dao.py
@@ -0,0 +1,51 @@
+from sqlalchemy.orm.exc import UnmappedInstanceError
+from src.dao.product_dao import ProductDao
+from src.models.product import Product
+import pytest
+
+
+class TestProductDao:
+    def test_instance(self):
+        product_dao = ProductDao()
+        assert isinstance(product_dao, ProductDao)
+
+    def test_save(self):
+        product = Product('Um nome', 5.99, 'Uma descrição')
+        product_saved = ProductDao().save(product)
+
+        assert product_saved.id_ is not None
+        ProductDao().delete(product_saved)
+
+    def test_not_save(self):
+        with pytest.raises(UnmappedInstanceError):
+            product_saved = ProductDao().save('product')
+
+    def test_read_by_id(self):
+        product = Product('Um nome', 5.99, 'Uma descrição')
+        product_saved = ProductDao().save(product)
+        product_read = ProductDao().read_by_id(product_saved.id_)
+
+        assert isinstance(product_read, Product)
+        ProductDao().delete(product_read)
+
+    def test_not_read_by_id(self):
+        with pytest.raises(TypeError):
+            product_read = ProductDao().read_by_id('product_saved.id_')
+
+    def test_read_all(self):
+        product_read = ProductDao().read_all()
+
+        assert isinstance(product_read, list)
+
+    def test_delete(self):
+        product = Product('Um nome', 5.99, 'Uma descrição')
+        product_saved = ProductDao().save(product)
+        product_read = ProductDao().read_by_id(product_saved.id_)
+        ProductDao().delete(product_read)
+        product_read = ProductDao().read_by_id(product_saved.id_)
+
+        assert product_read is None
+
+    def test_not_delete(self):
+        with pytest.raises(UnmappedInstanceError):
+            ProductDao().delete('product_read')
