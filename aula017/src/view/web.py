19e4b31d92ba7658fd962b78de90687c7ed74bd7
rafaela.teixeira <rafaellynha.a@hotmail.com>
2021-02-03 16:00:07 -0300

    criacao camada visul web

---
+++ aula017/src/view/web.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/sport')
+def sport():
+    return render_template('sport.html')
+
+@app.route('/customer')
+def customer():
+    return render_template('customer.html')
+
+
+app.run(debug=True)
