2bcc63cbe4f7323e2b4ba618d830f8762f34e0a3
Marcello Santana <marcellosantana@live.com>
2021-02-03 11:47:32 -0300

    refatoração dos testes de bet utilizando fixture

--- aula017/src/tests/models/test_bet.py
+++ aula017/src/tests/models/test_bet.py
@@ -1,29 +1,33 @@
 from src.models.bet import Bet
 import pytest
 
-def test_id_customer_not_null():
-    with pytest.raises(TypeError):
-        bet = Bet(None, 1, 1, 10.0)
+class TestBetDao:
+    @pytest.fixture
+    def create_instance(self):
+        bet = Bet(1, 1, 0, 10.0)
+        return bet
+    def test_id_customer_not_null(self, create_instance):
+        with pytest.raises(TypeError):
+            create_instance.id_customer = None
 
-def test_id_match_not_null():
-    with pytest.raises(TypeError):
-        bet = Bet(1, None, 1, 10.0)
+    def test_id_match_not_null(self, create_instance):
+        with pytest.raises(TypeError):
+            create_instance.id_match = None
 
-def test_id_team_sport_not_null():
-    with pytest.raises(TypeError):
-        bet = Bet(1, 1, None, 10.0)
+    def test_id_team_sport_not_null(self, create_instance):
+        with pytest.raises(TypeError):
+            create_instance.id_team_sport = None
 
-def test_bet_value_not_null():
-    with pytest.raises(TypeError):
-        bet = Bet(1, 1, 0, None)
+    def test_bet_value_not_null(self, create_instance):
+        with pytest.raises(TypeError):
+            create_instance.bet_value = None
 
-def test_bet_value_greater_than_0():
-    with pytest.raises(ValueError):
-        bet = Bet(1, 1, 0, -1.0)
+    def test_bet_value_greater_than_0(self, create_instance):
+        with pytest.raises(ValueError):
+            create_instance.bet_value = -1.0
 
-def test_create_valid_bet():
-    bet = Bet(1, 1, 0, 10.0)
-    assert isinstance(bet, Bet)
+    def test_create_valid_bet(self, create_instance):
+        assert isinstance(create_instance, Bet)
 
 
 
