470b2258f3d7d48b656677959b9871613b6ccfb2
Matheus Abreu <matheus.abreu@olist.com>
2021-02-04 15:59:33 -0300

    Created new Api layer

---
+++ aula017/src/view2/api.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Api
+
+from src.resources.customer_resource import CustomerResource
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(CustomerResource, '/api/customer', endpoint='customers')
+api.add_resource(CustomerResource, '/api/customer/<int:id>', endpoint='customer')
+
+
+@app.route('/')
+def index():
+    return 'Bem Vindo!'
+
+
+app.run(debug=True)
