3e314c622c8cf102e62a2811358ff89c7bb2035b
Marcello Santana <marcellosantana@live.com>
2021-02-04 10:59:59 -0300

    reupando arquivos

--- aula017/src/view/web.py
+++ aula017/src/view/web.py
@@ -43,11 +43,6 @@ def sport_create():
     return redirect('/sport')
 
 
-@app.route('/customer')
-def customer():
-    return render_template('customer.html')
-
-
 @app.route('/product')
 def product():
     controller = ProductController()
@@ -67,5 +62,22 @@ def create_product():
         return redirect('/product')       
     return render_template('product_create.html')
 
+@app.route('/customer')
+def customer():
+    controller_customer = CustomerController()
+    list_customer = controller_customer.read_all()
+    return render_template('customer.html', list_customer = list_customer)
+
+@app.route('/create_customer', methods=['GET','POST'])
+def create_customer():
+    if request.form.get('name'):
+        name = request.form.get('name')
+        num_doc = request.form.get('identification')
+        phone = request.form.get('phone')
+        controller_customer = CustomerController()
+        customer = Customer(name,num_doc,phone)
+        controller_customer.create(customer)
+        return redirect('/customer')
+    return render_template('form_create_customer.html')
 
 app.run(debug=True)
