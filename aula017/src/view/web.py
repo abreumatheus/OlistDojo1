259f82d7dfb6c4b8da5f28b258c2eca05ea35c71
luciheit@gmail.com <luciheit@gmail.com>
2021-02-04 10:49:57 -0300

    created product routes

--- aula017/src/view/web.py
+++ aula017/src/view/web.py
@@ -1,7 +1,12 @@
+import sys
+sys.path.append('.')
+
 from flask import Flask, render_template, request, redirect
 
 from src.controllers.sport_controller import SportController
+from src.controllers.product_controller import ProductController
 from src.models.sport import Sport
+from src.models.product import Product
 
 app = Flask(__name__)
 
@@ -45,7 +50,22 @@ def customer():
 
 @app.route('/product')
 def product():
-    return render_template('product.html')
+    controller = ProductController()
+    product_list = controller.read_all()
+    return render_template('product.html', products=product_list)
+
+
+@app.route('/product/create', methods=['POST', 'GET'])
+def create_product():
+    if request.method == 'POST':
+        controller = ProductController()
+        name = request.form.get('name')
+        description = request.form.get('description')
+        price = float(request.form.get('price'))
+        new_product = Product(name, price, description)
+        controller.create(new_product)
+        return redirect('/product')       
+    return render_template('product_create.html')
 
 
 app.run(debug=True)
