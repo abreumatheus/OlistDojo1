382ce9b0d20b7fb163a90ad2e4bdef80c4254ea7
Ralph <ralph.souza@olist.com>
2021-02-12 12:09:37 -0300

    Code review.

--- django_drf/product/admin.py
+++ django_drf/product/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'price', 'create_at', 'update_at')
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- django_drf/product/models.py
+++ django_drf/product/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.name} {self.description}'
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100, unique=True, null=False, blank=False)
     description = models.TextField()
     price = models.DecimalField(max_digits=9, decimal_places=2, null=False)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT, null=True)
 
     def __str__(self):
         return f'{self.name} {self.description} {self.price}'
